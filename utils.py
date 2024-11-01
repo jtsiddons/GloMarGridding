5337af10aae9e3926343540a8b96a82687dbc69a
josidd <joseph.siddons@noc.ac.uk>
2024-11-01 11:38:56 +0000

    chore: format

--- utils.py
+++ utils.py
@@ -4,15 +4,12 @@ import polars as pl
 from datetime import date
 
 
-def add_empty_layers(
-    nc_variables: list[nc.Variable] | nc.Variable,
-    timestamps: list[int] | int,
-    shape: tuple[int, int],
-) -> None:
+def add_empty_layers(nc_variables: list[nc.Variable] | nc.Variable,
+                     timestamps: list[int] | int,
+                     shape: tuple[int, int],
+                     ) -> None:
     empty = np.zeros(shape=shape).astype(np.float32)
-    nc_variables = (
-        [nc_variables] if not isinstance(nc_variables, list) else nc_variables
-    )
+    nc_variables = [nc_variables] if not isinstance(nc_variables, list) else nc_variables
     timestamps = [timestamps] if not isinstance(timestamps, list) else timestamps
     for variable in nc_variables:
         for timestamp in timestamps:
