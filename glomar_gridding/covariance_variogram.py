a28ba44741c686f4a240f2e8d63a46234d7dd18a
josidd <joseph.siddons@noc.ac.uk>
2025-01-02 11:47:08 +0000

    chore: line-length

--- glomar_gridding/covariance_variogram.py
+++ glomar_gridding/covariance_variogram.py
@@ -43,8 +43,8 @@ def linear_variogram_model(m, d):
     """
     Linear model
     m is a list containing [slope, nugget]
-    d is an array of the distance values at which to calculate the variogram model
-    from: Code by Benjamin S. Murphy and the PyKrige Developers
+    d is an array of the distance values at which to calculate the variogram
+    model from: Code by Benjamin S. Murphy and the PyKrige Developers
     bscott.murphy@gmail.com
     """
     warn(
@@ -84,8 +84,8 @@ def power_variogram_model(m, d):
     """
     Power model
     m is a list containing [scale, exponent, nugget]
-    d is an array of the distance values at which to calculate the variogram model
-    from: Code by Benjamin S. Murphy and the PyKrige Developers
+    d is an array of the distance values at which to calculate the variogram
+    model from: Code by Benjamin S. Murphy and the PyKrige Developers
     bscott.murphy@gmail.com
     """
     warn(
@@ -136,8 +136,8 @@ def gaussian_variogram_model(m, d):
     """
     Gaussian model
     m is a list containing [psill, range, nugget]
-    d is an array of the distance values at which to calculate the variogram model
-    from: Code by Benjamin S. Murphy and the PyKrige Developers
+    d is an array of the distance values at which to calculate the variogram
+    model from: Code by Benjamin S. Murphy and the PyKrige Developers
     bscott.murphy@gmail.com
     """
     warn(
@@ -181,8 +181,8 @@ def exponential_variogram_model(m, d):
     """
     Exponential model
     m is a list containing [psill, range, nugget]
-    d is an array of the distance values at which to calculate the variogram model
-    from: Code by Benjamin S. Murphy and the PyKrige Developers
+    d is an array of the distance values at which to calculate the variogram
+    model from: Code by Benjamin S. Murphy and the PyKrige Developers
     bscott.murphy@gmail.com
     """
     warn(
@@ -286,14 +286,18 @@ def matern_variogram_model_classic(m, d, nu=0.5):
         # Prefered style for pep/Python style guidelines
         return matern_variogram_model_classic(m, d, nu=1.5)
     or
-    # The lazy way, but not recommended by pep style guidelines (does the same thing)
+    # The lazy way, but not recommended by pep style guidelines (does the same
+    # thing)
     matern_nu_eq_1p5 = lambda m, d: matern_variogram_model_classic(m, d, nu=1.5)
 
-    1) This is called ``classic'' because if d/range_ = 1.0 and nu=0.5, it gives 1/e correlation...
-    2) This is NOT the same formulation as in GSTAT nor in papers about non-stationary anistropic
-    covariance models (aka Karspeck paper).
-    3) It is perhaps the most intitutive (because of (1)) and is used in sklearn GP and HadCRUT5 and other UKMO dataset.
-    4) nu defaults to 0.5 (exponential; used in HADSST4 and our kriging). HadCRUT5 uses 1.5.
+    1) This is called ``classic'' because if d/range_ = 1.0 and nu=0.5, it gives
+       1/e correlation...
+    2) This is NOT the same formulation as in GSTAT nor in papers about
+       non-stationary anistropic covariance models (aka Karspeck paper).
+    3) It is perhaps the most intitutive (because of (1)) and is used in sklearn
+       GP and HadCRUT5 and other UKMO dataset.
+    4) nu defaults to 0.5 (exponential; used in HADSST4 and our kriging).
+       HadCRUT5 uses 1.5.
     5) The "2" is inside the square root for middle and right.
 
     Reference; see chapter 4.2 of:
@@ -381,7 +385,8 @@ def find_values(ds_masked_xr, lat, lon, timestep):
     cci (array) - array of cci  vaules for each point in the whole domain
     lat (array) - array of latitudes of the observations
     lon (array) - array of longitudes of the observations
-    df (dataframe) - dataframe containing all information for a given day (such as location, measurement values)
+    df (dataframe) - dataframe containing all information for a given day (such
+      as location, measurement values)
 
     Returns
     -------
@@ -389,13 +394,15 @@ def find_values(ds_masked_xr, lat, lon, timestep):
     """
     cci_lat_idx = find_nearest(ds_masked_xr.lat, lat)
     print(ds_masked_xr.lat, cci_lat_idx)
-    # problem with this right here (see email to Liz and Richard) - for both lat and lon
+    # problem with this right here (see email to Liz and Richard) - for both
+    # lat and lon
     # see: https://stackoverflow.com/questions/40592630/get-coordinates-of-non-nan-values-of-xarray-dataset
     cci_lon_idx = find_nearest(ds_masked_xr.lon, lon)
     print(ds_masked_xr.lon, cci_lon_idx)
     cci_vals = []  # fake ship obs
     for i in range(len(cci_lat_idx)):
-        # cci_ = cci[str(ds_var)].values[timestep,cci_lat_idx[i],cci_lon_idx[i]] #was cci.sst_anomaly
+        # cci_ = cci[str(ds_var)].values[timestep,cci_lat_idx[i],cci_lon_idx[i]]
+        # was cci.sst_anomaly
         cci_ = ds_masked_xr.values[
             cci_lat_idx[i], cci_lon_idx[i], timestep
         ]  # was cci.sst_anomaly
@@ -430,7 +437,8 @@ def getDistanceByEuclidean(
     Calculate the Euclidean distance in kilometers between two points
     on the earth (specified in decimal degrees)
 
-    Input: latitude and longitude columns from Pandas dataframe (arrays of values)
+    Input: latitude and longitude columns from Pandas dataframe (arrays of
+      values)
     Output: a 2D numpy array of (lat_decimal,lon_decimal) pairs
     """
     lat1 = loc1[0]
@@ -461,7 +469,8 @@ def getDistanceByHaversine(
     Calculate the great circle distance in kilometers between two points
     on the earth (specified in decimal degrees)
 
-    Input: latitude and longitude columns from Pandas dataframe (arrays of values)
+    Input: latitude and longitude columns from Pandas dataframe (arrays of
+      values)
     Output: a 2D numpy array of (lat_decimal,lon_decimal) pairs
     """
     # "unpack" our numpy array, this extracts column wise arrays
@@ -492,27 +501,32 @@ def calculate_distance_matrix(df, dist_func=getDistanceByHaversine):
 
 
 def variogram(
-    distance_matrix,
-    variance,
-    nugget_=0.0,
-    range_=350.0,
-    variogram_model=exponential_variogram_model,
-):
+    distance_matrix: np.ndarray,
+    variance: np.ndarray,
+    nugget_: float = 0.0,
+    range_: float = 350.0,
+    variogram_model: Callable = exponential_variogram_model,
+) -> np.ndarray:
     # range from Dave's presentation on space length scales (in km)
     # range_ = 350
-    # from researchgate - Sill of the semivariogram is equal to the variance of the random variable, at large distances, when variables become uncorrelated
+    # from researchgate - Sill of the semivariogram is equal to the variance of
+    # the random variable, at large distances, when variables become
+    # uncorrelated
     sill_ = variance
-    # nugget for now can be set to zero, it will change once we quantify the obs uncertainty better
+    # nugget for now can be set to zero, it will change once we quantify the
+    # obs uncertainty better
     # nugget_ = 0
 
     # create m - a list containing [psill, range, nugget]
     m = [sill_, range_, nugget_]
 
-    # create d - an array of the distance values at which to calculate the variogram model
+    # create d - an array of the distance values at which to calculate the
+    # variogram model
     d = distance_matrix
 
     # call variogram function
-    # this calculates the covariance between the observations only (equivalemnt in Simon's code is "S" martix)
+    # this calculates the covariance between the observations only (equivalemnt
+    # in Simon's code is "S" martix)
     obs_covariance = variogram_model(m, d)
     return obs_covariance
 
@@ -541,109 +555,109 @@ def variogram_to_covariance(variogram, variance):
     return variance - variogram
 
 
-"""
-######################
-# read in the observational data
-######################
-filename = pd.read_csv(sys.argv[1]) # netcdf file #/noc/users/agfaul/data_nc/MAT_1820_7.nc
-lats = filename['lat'].values #np.asarray(nc.Dataset(filename).variables['lat']) #it's a 1-D array based on the psv file
-lons = filename['lon'].values #np.asarray(nc.Dataset(filename).variables['lon']) #it's a 1-D array based on the psv file
-#coords = list(zip(list(lats),list(lons)))
-vals = filename['anomaly'].values #np.asarray(nc.Dataset(filename).variables['anomaly']) #it's a 1-D array based on the psv file
+# QUESTION: Can I delete the following?
 
-print('LATS', lats, len(lats))
-print('LONS', lons, len(lons))
-#print('COORDS', coords)
-print('VALS', vals, len(vals))
-
-
-
-##################
-# compute pairwise distances
-##################
-
-# distance matrix using pandas
-data = {'lat': lats, 'lon': lons}
-df = pd.DataFrame(data, columns=['lat', 'lon'])
-
-distance_matrix = pd.DataFrame(squareform(pdist(df, lambda u, v: getDistanceByHaversine(u,v))), index=df.index, columns=df.index)
-
-print('Distance matrix \n', distance_matrix)
-
-
-#####################
-# compute the raw variogram values
-#####################
-dissim = np.abs(vals[:, None] - vals[None, :])**2 / 2
-dissim_ = squareform(dissim)
-
-variance = np.var(vals)
-print('VARIANCE', variance)
-
-
-#####################
-# set nugget, sill and range
-#####################
-
-#range from Dave's presentation on space length scales (in km)
-range_ = 350 
-#from researchgate - Sill of the semivariogram is equal to the variance of the random variable, at large distances, when variables become uncorrelated
-sill_ = variance 
-#nugget for now can be set to zero, it will change once we quantify the obs uncertainty better
-nugget_ = 0 
-
-#create m - a list containing [psill, range, nugget]
-m = [sill_, range_, nugget_]
-
-#create d - an array of the distance values at which to calculate the variogram model
-d = distance_matrix
-
-#call variogram function
-#this calculates the covariance between the observations only (equivalemnt in Simon's code is "S" martix
-obs_covariance = exponential_variogram_model(m, d)
-
-print(variogram)
-print(variogram.shape)
-
-
-######################
-# set up a global grid for distance matrix computation
-#####################
-
-#create global grid for finding prediction points                                                          
-global_lats = np.arange(-90., 90.,1.)
-global_lons = np.arange(-180., 180.,1.)
-
-#calculate the covariance between the observation points and the prediction points (equivlent in Simon's code is the "Ss" matrix
-
-global_data = {'lat': global_lats, 'lon': global_lons}
-global_df = pd.DataFrame(global_data, columns=['lat', 'lon'])
-global_distance_matrix = pd.DataFrame(distance_matrix(global_df.values, global_df.values), index=global_df.index, columns=global_df.index)
-print('global distance_matrix \n', global_distance_matrix)
-
-d_global = global_distance_matrix
-global_covariance = exponential_variogram_model(m, d_global)
-
-
-timestep = 0
-ds_masked_xr = xr.DataArray(ds_masked, coords=cci_pentad_files.coords, dims=cci_pentad_files.dims, attrs=cci_pentad_files.attrs)
-#on a global 1-D grid find the index of where the observations are
-iid = watermask(ds_masked_xr,timestep)
-#extract the CCI SST anomaly values corresponding to the shop lat/lon coordinate points                       
-#fake_ship_obs = find_nearest(ds.lat.values, ship_lat)                                                      
-fake_ship_obs, lat_idx, lon_idx = find_values(ds_masked_xr, ship_lat, ship_lon, timestep)
-cond_df['cci_anomalies'] = fake_ship_obs
-#in case some of the values are Nan (because covered by ice)                                                
-cond_df = cond_df.dropna()
-idx_tuple = np.array([lat_idx, lon_idx])
-#output_size = (ds[str(ds_var)].values[timestep,:,:]).shape                                                 
-output_size = (ds_masked_xr.values[:,:,timestep]).shape
-flattened_idx = np.ravel_multi_index(idx_tuple, output_size, order='C') #row-major; order='F') column-major
-uind = np.unique(flattened_idx)
-
-_,ia,_ = intersect_mtlb(iid,uind)
-obs_unknown_covariance = global_covariance[ia,:]
-
-#the rest should be the same in kriging
-#idea: could try the kriging code with weights provided by the https://gis.stackexchange.com/questions/270274/how-to-calculate-kriging-weights/301362#301362 and see how different they are
-"""
+# ######################
+# # read in the observational data
+# ######################
+# filename = pd.read_csv(sys.argv[1]) # netcdf file #/noc/users/agfaul/data_nc/MAT_1820_7.nc
+# lats = filename['lat'].values #np.asarray(nc.Dataset(filename).variables['lat']) #it's a 1-D array based on the psv file
+# lons = filename['lon'].values #np.asarray(nc.Dataset(filename).variables['lon']) #it's a 1-D array based on the psv file
+# #coords = list(zip(list(lats),list(lons)))
+# vals = filename['anomaly'].values #np.asarray(nc.Dataset(filename).variables['anomaly']) #it's a 1-D array based on the psv file
+#
+# print('LATS', lats, len(lats))
+# print('LONS', lons, len(lons))
+# #print('COORDS', coords)
+# print('VALS', vals, len(vals))
+#
+#
+#
+# ##################
+# # compute pairwise distances
+# ##################
+#
+# # distance matrix using pandas
+# data = {'lat': lats, 'lon': lons}
+# df = pd.DataFrame(data, columns=['lat', 'lon'])
+#
+# distance_matrix = pd.DataFrame(squareform(pdist(df, lambda u, v: getDistanceByHaversine(u,v))), index=df.index, columns=df.index)
+#
+# print('Distance matrix \n', distance_matrix)
+#
+#
+# #####################
+# # compute the raw variogram values
+# #####################
+# dissim = np.abs(vals[:, None] - vals[None, :])**2 / 2
+# dissim_ = squareform(dissim)
+#
+# variance = np.var(vals)
+# print('VARIANCE', variance)
+#
+#
+# #####################
+# # set nugget, sill and range
+# #####################
+#
+# #range from Dave's presentation on space length scales (in km)
+# range_ = 350
+# #from researchgate - Sill of the semivariogram is equal to the variance of the random variable, at large distances, when variables become uncorrelated
+# sill_ = variance
+# #nugget for now can be set to zero, it will change once we quantify the obs uncertainty better
+# nugget_ = 0
+#
+# #create m - a list containing [psill, range, nugget]
+# m = [sill_, range_, nugget_]
+#
+# #create d - an array of the distance values at which to calculate the variogram model
+# d = distance_matrix
+#
+# #call variogram function
+# #this calculates the covariance between the observations only (equivalemnt in Simon's code is "S" martix
+# obs_covariance = exponential_variogram_model(m, d)
+#
+# print(variogram)
+# print(variogram.shape)
+#
+#
+# ######################
+# # set up a global grid for distance matrix computation
+# #####################
+#
+# #create global grid for finding prediction points
+# global_lats = np.arange(-90., 90.,1.)
+# global_lons = np.arange(-180., 180.,1.)
+#
+# #calculate the covariance between the observation points and the prediction points (equivlent in Simon's code is the "Ss" matrix
+#
+# global_data = {'lat': global_lats, 'lon': global_lons}
+# global_df = pd.DataFrame(global_data, columns=['lat', 'lon'])
+# global_distance_matrix = pd.DataFrame(distance_matrix(global_df.values, global_df.values), index=global_df.index, columns=global_df.index)
+# print('global distance_matrix \n', global_distance_matrix)
+#
+# d_global = global_distance_matrix
+# global_covariance = exponential_variogram_model(m, d_global)
+#
+#
+# timestep = 0
+# ds_masked_xr = xr.DataArray(ds_masked, coords=cci_pentad_files.coords, dims=cci_pentad_files.dims, attrs=cci_pentad_files.attrs)
+# #on a global 1-D grid find the index of where the observations are
+# iid = watermask(ds_masked_xr,timestep)
+# #extract the CCI SST anomaly values corresponding to the shop lat/lon coordinate points
+# #fake_ship_obs = find_nearest(ds.lat.values, ship_lat)
+# fake_ship_obs, lat_idx, lon_idx = find_values(ds_masked_xr, ship_lat, ship_lon, timestep)
+# cond_df['cci_anomalies'] = fake_ship_obs
+# #in case some of the values are Nan (because covered by ice)
+# cond_df = cond_df.dropna()
+# idx_tuple = np.array([lat_idx, lon_idx])
+# #output_size = (ds[str(ds_var)].values[timestep,:,:]).shape
+# output_size = (ds_masked_xr.values[:,:,timestep]).shape
+# flattened_idx = np.ravel_multi_index(idx_tuple, output_size, order='C') #row-major; order='F') column-major
+# uind = np.unique(flattened_idx)
+#
+# _,ia,_ = intersect_mtlb(iid,uind)
+# obs_unknown_covariance = global_covariance[ia,:]
+#
+# #the rest should be the same in kriging
+# #idea: could try the kriging code with weights provided by the https://gis.stackexchange.com/questions/270274/how-to-calculate-kriging-weights/301362#301362 and see how different they are
