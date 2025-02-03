f1488fc12e5b90fb6900df5197d0836905fa8ca6
josidd <joseph.siddons@noc.ac.uk>
2025-02-03 09:31:36 +0000

    refactor(climatology): use polars operations, convert climatology array to frame

--- glomar_gridding/climatology.py
+++ glomar_gridding/climatology.py
@@ -1,6 +1,5 @@
 """Functions for mapping climatologies and computing anomalies"""
 
-import numpy as np
 import polars as pl
 import xarray as xr
 from glomar_gridding.utils import find_nearest
@@ -17,6 +16,7 @@ def join_climatology_by_doy(
     clim_lon: str = "longitude",
     clim_doy: str = "doy",
     clim_var: str = "climatology",
+    temp_from_kelvin: bool = True,
 ) -> pl.DataFrame:
     """
     Merge a climatology from an xarray.DataArray into a polars.DataFrame using
@@ -54,6 +54,9 @@ def join_climatology_by_doy(
         Name of the day of year coordinate in the climatology DataArray.
     clim_var : str
         Name of the climatology variable in the climatology DataArray.
+    temp_from_kelvin : bool
+        Optionally adjust the climatology from Kelvin to Celcius if the variable
+        is a temperature.
 
     Returns
     -------
@@ -62,49 +65,75 @@ def join_climatology_by_doy(
         "_climatology" and "_anomaly" prefixed by the `var_col` value
         respectively.
     """
+    # Names of the output columns
+    clim_var_name = f"{var_col}_climatology"
+    anom_var_name = f"{var_col}_anomaly"
+
+    climatology = pl.from_pandas(
+        climatology_365[clim_var].to_dataframe().reset_index(drop=False)
+    )
+    if temp_from_kelvin:
+        climatology = climatology.with_columns(
+            (pl.col(clim_var) - 273.15).name.keep()
+        )
+    climatology = climatology.select([clim_lat, clim_lon, clim_doy, clim_var])
+
     obs_lat = obs_df.get_column(lat_col)
-    lat_idx, _ = find_nearest(climatology_365.coords[clim_lat], obs_lat)
+    _, lat_vals = find_nearest(climatology_365.coords[clim_lat], obs_lat)
 
     obs_lon = obs_df.get_column(lon_col)
-    lon_idx, _ = find_nearest(climatology_365.coords[clim_lon], obs_lon)
+    _, lon_vals = find_nearest(climatology_365.coords[clim_lon], obs_lon)
+
+    obs_df = obs_df.with_columns(
+        pl.Series("clim_lat", lat_vals),
+        pl.Series("clim_lon", lon_vals),
+    )
 
     mask = (obs_df.get_column(date_col).dt.is_leap_year()) & (
         obs_df.get_column(date_col).dt.ordinal_day().eq(60)
     )
-    non_leap_df = obs_df.filter(~mask)
-    leap_df = obs_df.filter(mask)
-    non_leap_df = non_leap_df.with_columns(
-        pl.datetime(pl.lit(2009), pl.col("mo"), pl.col("dy"))
-        .dt.ordinal_day()
-        .alias("doy")
+
+    non_leap_df = (
+        obs_df.filter(~mask)
+        .with_columns(
+            pl.datetime(pl.lit(2009), pl.col("mo"), pl.col("dy"))
+            .dt.ordinal_day()
+            .alias("doy")
+        )
+        .join(
+            climatology,
+            left_on=["clim_lat", "clim_lon", "doy"],
+            right_on=[clim_lat, clim_lon, clim_doy],
+            how="left",
+            coalesce=True,
+        )
+        .drop(["clim_lat", "clim_lon", "doy"])
     )
-    doy_idx = non_leap_df.get_column("doy") - 1
 
-    climatology = climatology_365[clim_var].values
-    climatology = climatology - 273.15
+    # Take average of 28th Feb and 1st March for 29th Feb
+    leap_clim = (
+        climatology.filter(pl.col(clim_doy).is_between(59, 60, closed="both"))
+        .group_by([clim_lat, clim_lon])
+        .agg(pl.col(clim_var).mean())
+    )
 
-    # TODO: Dynamic ordering of indices
-    selected = climatology[doy_idx, lat_idx, lon_idx]
+    leap_df = (
+        obs_df.filter(mask)
+        .join(
+            leap_clim,
+            left_on=["clim_lat", "clim_lon"],
+            right_on=[clim_lat, clim_lon],
+            how="left",
+            coalesce=True,
+        )
+        .drop(["clim_lat", "clim_lon"])
+    )
 
-    non_leap_df["climatology"] = selected
-    end_feb = climatology[
-        np.repeat(np.array([58]), leap_df.height),
-        lat_idx,
-        lon_idx,
-    ]
+    del climatology, leap_clim
 
-    beg_mar = climatology[
-        np.repeat(np.array([59]), leap_df.height),
-        lat_idx,
-        lon_idx,
-    ]
-
-    selected2 = [(g + h) / 2 for g, h in zip(end_feb, beg_mar)]
-
-    clim_var_name = f"{var_col}_climatology"
-    anom_var_name = f"{var_col}_anomaly"
-    leap_df[clim_var_name] = selected2
     obs_df = pl.concat([non_leap_df, leap_df])
-    obs_df[anom_var_name] = obs_df[var_col] - obs_df[clim_var_name]
+    obs_df = obs_df.with_columns(
+        (pl.col(var_col) - pl.col(clim_var_name)).alias(anom_var_name)
+    )
 
     return obs_df.drop_nulls()
