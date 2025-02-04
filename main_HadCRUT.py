0020ba2f184e04c47eb01bf968de39c11ffdc207
josidd <joseph.siddons@noc.ac.uk>
2025-02-04 08:05:30 +0000

    refactor: main_HadCRUT.py now uses yaml config, and updated functions

--- main_HadCRUT.py
+++ main_HadCRUT.py
@@ -1,38 +1,137 @@
 #!/usr/bin/env python
+"""
+Script to run Kriging for HadCRUT.
 
-################
-# by A. Faulkner
-# for python version 3.0 and up
-################
+By A. Faulkner for python version 3.0 and up.
+"""
 
 # global
 import os
 
 # argument parser
 import argparse
-from configparser import ConfigParser
+import yaml
 
 # math tools
 import numpy as np
 
 # data handling tools
-from pandas.core.groupby import DataFrameGroupBy
+import polars as pl
 import xarray as xr
 import netCDF4 as nc
 
 # self-written modules (from the same directory)
-import glomar_gridding.observations as obs_module
-import glomar_gridding.kriging as krig
-from glomar_gridding.utils import days_since_by_month, ConfigParserMultiValues
+from glomar_gridding.grid import (
+    align_to_grid,
+    assign_to_grid,
+    grid_from_resolution,
+)
+from glomar_gridding.error_covariance import get_weights
+from glomar_gridding.kriging import kriging
+from glomar_gridding.utils import days_since_by_month
 
 import warnings
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-config",
+    dest="config",
+    required=False,
+    default=os.path.join(os.path.dirname(__file__), "config_HadCRUT.ini"),
+    help="Path to INI file containing configuration settings",
+    type=str,
+)
+parser.add_argument(
+    "-year_start",
+    dest="year_start",
+    required=False,
+    help="start year",
+    type=int,
+)
+parser.add_argument(
+    "-year_stop",
+    dest="year_stop",
+    required=False,
+    help="end year",
+    type=int,
+)
+parser.add_argument(
+    "-member",
+    dest="member",
+    required=False,
+    help="ensemble member: required argument",
+    type=int,
+    default=0,
+)
+parser.add_argument(
+    "-variable",
+    dest="variable",
+    required=True,
+    help='variable to process: "sst" or "lsat"',
+    type=str,
+    choices=["sst", "lsat"],
+)
+parser.add_argument(
+    "-method",
+    dest="method",
+    default="ordinary",
+    required=False,
+    help='Kriging Method - one of "simple" or "ordinary"',
+    choices=["simple", "ordinary"],
+)
+parser.add_argument(
+    "-interpolation",
+    dest="interpolation",
+    default="ellipse",
+    required=False,
+    help='Interpolation covariance - one of "distance" or "ellipse"',
+    choices=["distance", "ellipse"],
+)
+
+parser.add_argument(
+    "-remove_obs_mean",
+    dest="remove_obs_mean",
+    default=0,
+    required=False,
+    type=int,
+    help="Should the global mean be removed? - 0:no, 1:yes, "
+    + "2:yes but median, 3:yes but spatial mean",
+    choices=[0, 1, 2, 3],
+)
+
+
+def _parse_args(parser) -> tuple[dict, dict, int, int, int, str, str, str, int]:
+    args = parser.parse_args()
+    config: dict = yaml.safe_load(args.config)
+    year_start: int = args.year_start or config.get("domain", {}).get(
+        "startyear", 1850
+    )
+    year_stop: int = args.year_stop or config.get("domain", {}).get(
+        "endyear", 2023
+    )
+    variable = args.variable
+    var_config: dict = config.get(variable, {})
+    if not var_config:
+        raise ValueError(f"Cannot get variable configuration for {variable}")
+
+    return (
+        config,
+        var_config,
+        year_start,
+        year_stop,
+        args.member,
+        variable,
+        args.method,
+        args.interpolation,
+        args.remove_obs_mean,
+    )
 
 
 def _get_sst_err_cov(
     current_year: int,
     current_month: int,
     error_covariance_path: str,
-    uncorrelated: xr.Dataset,
+    # uncorrelated: xr.Dataset,
 ) -> np.ndarray:
     err_cov_fn = (
         f"HadCRUT.5.0.2.0.error_covariance.{current_year}{current_month:02d}.nc"
@@ -44,19 +143,35 @@ def _get_sst_err_cov(
 
 
 def _get_lsat_err_cov(
-    current_year: int, current_month: int, error_cov: np.ndarray
+    current_year: int,
+    current_month: int,
+    error_cov: np.ndarray,
 ) -> np.ndarray:
     date_int = (current_year - 1850) * 12 + (current_month - 1)
     return np.diag(error_cov[date_int, :])
 
 
-def _get_obs_groups(data_path: str, var: str) -> DataFrameGroupBy:
-    obs = xr.open_dataset(data_path).to_dataframe().reset_index()
-    obs.rename(columns={"latitude": "lat", "longitude": "lon"}, inplace=True)
-    obs.drop_duplicates(subset=["lon", "lat", "time", var], inplace=True)
-    obs.dropna(subset=[var], inplace=True)
-    obs.reset_index(inplace=True)
-    return obs.groupby([obs.time.dt.year, obs.time.dt.month])
+def _get_obs_groups(
+    data_path: str,
+    var: str,
+) -> dict[tuple[object, ...], pl.DataFrame]:
+    if not os.path.isfile(data_path):
+        raise FileNotFoundError(f"Cannot find observations file {data_path}.")
+    obs = pl.from_pandas(
+        xr.open_dataset(data_path).to_dataframe().reset_index()
+    )
+    obs = (
+        obs.rename({"latitude": "lat", "longitude": "lon"})
+        .unique(subset=["lon", "lat", "time", var])
+        .filter(pl.col(var).is_not_nan() & pl.col(var).is_not_null())
+        .with_columns(
+            [
+                pl.col("time").dt.year().alias("year"),
+                pl.col("time").dt.month().alias("month"),
+            ]
+        )
+    )
+    return obs.partition_by(by=["year", "month"], as_dict=True)
 
 
 def _initialise_ncfile(
@@ -109,92 +224,21 @@ def _initialise_ncfile(
     return lon, lat, time, krig_anom, krig_uncert, grid_obs
 
 
-def main():  # noqa: D103
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-config",
-        dest="config",
-        required=False,
-        default=os.path.join(os.path.dirname(__file__), "config_HadCRUT.ini"),
-        help="Path to INI file containing configuration settings",
-        type=str,
-    )
-    parser.add_argument(
-        "-year_start",
-        dest="year_start",
-        required=False,
-        help="start year",
-        type=int,
-    )
-    parser.add_argument(
-        "-year_stop",
-        dest="year_stop",
-        required=False,
-        help="end year",
-        type=int,
-    )
-    parser.add_argument(
-        "-member",
-        dest="member",
-        required=False,
-        help="ensemble member: required argument",
-        type=int,
-        default=0,
-    )
-    parser.add_argument(
-        "-variable",
-        dest="variable",
-        required=True,
-        help='variable to process: "sst" or "lsat"',
-        type=str,
-        choices=["sst", "lsat"],
-    )
-    parser.add_argument(
-        "-method",
-        dest="method",
-        default="ordinary",
-        required=False,
-        help='Kriging Method - one of "simple" or "ordinary"',
-        choices=["simple", "ordinary"],
-    )
-    parser.add_argument(
-        "-interpolation",
-        dest="interpolation",
-        default="ellipse",
-        required=False,
-        help='Interpolation covariance - one of "distance" or "ellipse"',
-        choices=["distance", "ellipse"],
-    )
+def main():  # noqa: C901, D103
+    (
+        config,
+        var_config,
+        year_start,
+        year_stop,
+        member,
+        variable,
+        method,
+        interpolation_covariance_type,
+        remove_obs_mean,
+    ) = _parse_args(parser)
 
-    parser.add_argument(
-        "-remove_obs_mean",
-        dest="remove_obs_mean",
-        default=0,
-        required=False,
-        type=int,
-        help="Should the global mean be removed? - 0:no, 1:yes, "
-        + "2:yes but median, 3:yes but spatial mean",
-        choices=[0, 1, 2, 3],
-    )
-
-    args = parser.parse_args()
-
-    config_file = args.config
-    print(config_file)
-
-    # load config options from ini file
-    # this is done using an ini config file, which is located in the same direcotry as the python code
-    # instantiate
-    # ===== MODIFIED =====
-    config = ConfigParser(
-        strict=False,
-        empty_lines_in_values=False,
-        dict_type=ConfigParserMultiValues,
-        converters={"list": ConfigParserMultiValues.getlist},
-    )
     # ===== MODIFIED =====
     # parce existing config file
-    config.read(config_file)
     print(config)
 
     # read values from auxiliary_files section
@@ -205,26 +249,29 @@ def main():  # noqa: D103
     # for list (multiple options for same key) use config.getlist
 
     # set boundaries for the domain
-    lon_west: float = config.getfloat("HadCRUT", "lon_west")  # -180.
-    lon_east: float = config.getfloat("HadCRUT", "lon_east")  # 180.
-    lat_south: float = config.getfloat("HadCRUT", "lat_south")  # -90.
-    lat_north: float = config.getfloat("HadCRUT", "lat_north")  # 90.
+    lon_west: float = config.get("domain", {}).get("west", -180.0)
+    lon_east: float = config.get("domain", {}).get("east", 180.0)
+    lat_south: float = config.get("domain", {}).get("south", -90.0)
+    lat_north: float = config.get("domain", {}).get("north", 90.0)
 
     # extract the latitude and longitude boundaries from user input
     lon_bnds, lat_bnds = (lon_west, lon_east), (lat_south, lat_north)
     print(lon_bnds, lat_bnds)
 
-    output_lat = np.arange(lat_bnds[0] + 2.5, lat_bnds[-1] + 2.5, 5)
-    output_lon = np.arange(lon_bnds[0] + 2.5, lon_bnds[-1] + 2.5, 5)
+    output_grid: xr.DataArray = grid_from_resolution(
+        resolution=5.0,
+        bounds=[
+            (lat_south + 2.5, lat_north + 2.5),
+            (lon_west + 2.5, lon_east + 2.5),
+        ],
+        coord_names=["lat", "lon"],
+    )
+    output_lat: np.ndarray = output_grid.coords["lat"].values
+    output_lon: np.ndarray = output_grid.coords["lon"].values
     print(f"{output_lat =}")
     print(f"{output_lon =}")
 
-    year_start: int = args.year_start or config.getint("HadCRUT", "startyear")
-    year_stop: int = args.year_stop or config.getint("HadCRUT", "endyear")
-    print(year_start, year_stop)
-
     # what variable is being processed
-    variable: str = args.variable
     hadcrut_var: str = "tos" if variable == "sst" else "tas"
 
     # path to output directory
@@ -234,48 +281,45 @@ def main():  # noqa: D103
         os.makedirs(output_directory)
     print(f"{output_directory = }")
 
-    # read in member from comand line
-    member: int = args.member
-
     # var_range = config.getfloat(variable, "range")
     # var_sigma = config.getfloat(variable, "sigma")
     # var_matern = config.getfloat(variable, "matern")
 
-    interpolation_covariance_type: str = args.interpolation
-    interpolation_covariance_path: str = config.get(
-        variable, f"{interpolation_covariance_type}_interpolation_covariance"
+    interpolation_covariance_path: str = var_config.get(
+        f"{interpolation_covariance_type}_interpolation_covariance", ""
     )
     if interpolation_covariance_type == "distance":
         interp_covariance = np.load(interpolation_covariance_path)
         print("loaded interpolation covariance")
         print(interp_covariance)
 
-    data_path: str = config.get(variable, "observations")
+    data_path: str = var_config.get("observations", "")
     yr_mo = _get_obs_groups(data_path, hadcrut_var)
 
-    error_covariance_path = config.get(variable, "error_covariance")
+    error_covariance_path: str = var_config.get("error_covariance", "")
 
     match variable:
         case "sst":
             print(f"Processing for variable {variable} | {hadcrut_var}")
-            if config.has_option("sst", "sampling_uncertainty"):
+            if "sampling_uncertainty" in var_config:
                 single_sigma_warn_msg = (
                     "Option sampling_uncertainty for sst is ignored. "
+                    + "HadCRUT5 only has a single uncorrelated sigma; "
+                    + "if you are using multiple uncorrelated sigmas "
+                    + "(e.g. HadSST4), combine them first."
                 )
-                single_sigma_warn_msg += (
-                    "HadCRUT5 only has a single uncorrelated sigma; "
-                )
-                single_sigma_warn_msg += "if you are using multiple uncorrelated sigmas (e.g. HadSST4), combine them first."
                 warnings.warn(single_sigma_warn_msg, DeprecationWarning)
-            uncorrelated_uncertainty = config.get(
-                variable, "uncorrelated_uncertainty"
-            )
+            # uncorrelated_uncertainty = config.get(
+            #     variable, "uncorrelated_uncertainty"
+            # )
 
-            uncorrelated = xr.open_dataset(uncorrelated_uncertainty)
+            # uncorrelated = xr.open_dataset(uncorrelated_uncertainty)
 
             def get_error_cov(year: int, month: int) -> np.ndarray:
                 return _get_sst_err_cov(
-                    year, month, error_covariance_path, uncorrelated
+                    year,
+                    month,
+                    error_covariance_path,  # uncorrelated
                 )
 
         case "lsat":
@@ -324,14 +368,18 @@ def main():  # noqa: D103
         lat[:] = output_lat  # ds.lat.values
         lon[:] = output_lon  # ds.lon.values
 
-        for current_month in month_list:
-            timestep = current_month - 1
+        for timestep, current_month in enumerate(month_list):
             print("Current month and year: ", (current_month, current_year))
 
-            ################
-            mon_df = yr_mo.get_group(
-                (current_year, current_month)
-            ).reset_index()
+            mon_df: pl.DataFrame = yr_mo.get(
+                (current_year, current_month), pl.DataFrame()
+            )
+            if mon_df.height == 0:
+                warnings.warn(
+                    f"Current year, month ({current_year}, {current_month}) "
+                    + "has no data. Skipping."
+                )
+                continue
             print(f"{mon_df = }")
 
             if interpolation_covariance_type == "ellipse":
@@ -363,54 +411,69 @@ def main():  # noqa: D103
             print("-----------------")
             # since we're not using any landmask for this run
             # the line below:
-            # cond_df, obs_flat_idx = obs_module.watermask_at_obs_locations(lon_bnds, lat_bnds, mon_df, mask_ds, mask_ds_lat, mask_ds_lon)
+            # cond_df, obs_flat_idx = obs_module.watermask_at_obs_locations(
+            #     lon_bnds, lat_bnds, mon_df, mask_ds, mask_ds_lat, mask_ds_lon
+            # )
             # mon_flat_idx = cond_df['flattened_idx'][:]
             # can be substituted with:
-            lat_idx, grid_lat = obs_module.find_nearest(output_lat, mon_df.lat)
-            lon_idx, grid_lon = obs_module.find_nearest(output_lon, mon_df.lon)
-
-            mon_df["grid_lat"] = grid_lat
-            mon_df["grid_lon"] = grid_lon
-            mon_df["lat_idx"] = lat_idx
-            mon_df["lon_idx"] = lon_idx
-
-            idx_tuple = np.array([lat_idx, lon_idx])
-            # print(f'{idx_tuple =}')
-            mon_flat_idx = np.ravel_multi_index(
-                idx_tuple, mesh_lat.shape, order="C"
-            )  # row-major
+            mon_df = align_to_grid(
+                mon_df, output_grid, grid_coords=["lat", "lon"]
+            )
+            # lat_idx, grid_lat = obs_module.find_nearest(
+            #     output_lat, mon_df.get_column("lat")
+            # )
+            # lon_idx, grid_lon = obs_module.find_nearest(
+            #     output_lon, mon_df.get_column("lon")
+            # )
+            #
+            # mon_df = mon_df.with_columns(
+            #     [
+            #         pl.Series("grid_lat", grid_lat),
+            #         pl.Series("grid_lon", grid_lon),
+            #         pl.Series("lat_idx", lat_idx),
+            #         pl.Series("lon_idx", lon_idx),
+            #     ]
+            # )
+            #
+            # idx_tuple = (lat_idx, lon_idx)
+            # # print(f'{idx_tuple =}')
+            # mon_flat_idx = np.ravel_multi_index(
+            #     idx_tuple, mesh_lat.shape, order="C"
+            # )  # row-major
             # print(f'{mon_flat_idx =}') #it's the same as ec_idx
             # print(f'{sorted(mon_flat_idx) =}')
 
             # diff = sorted(mon_flat_idx) - np.unique(mon_flat_idx)
-            # diff results in 0s as unique idx is the same as sorted mon_flat_idx
+            # diff results in 0s as unique idx is the same as sorted
+            # mon_flat_idx
 
-            mon_df["gridbox"] = mon_flat_idx
-            # print(mon_df)
-            mon_df["error_covariance_diagonal"] = error_covariance[
-                mon_flat_idx, mon_flat_idx
-            ]
-            print(mon_df)
-            mon_df = mon_df.dropna(subset=["error_covariance_diagonal"])
-            mon_df.reset_index(inplace=True, drop=True)
+            # mon_df["gridbox"] = mon_flat_idx
+            # # print(mon_df)
+            error_cov_diag_at_obs = pl.Series(
+                "error_covariance_diagonal",
+                np.diag(error_covariance)[mon_df.get_column("grid_idx")],
+            )
+            mon_df = mon_df.with_columns(error_cov_diag_at_obs)
+            mon_df = mon_df.filter(
+                pl.col("error_covariance_diagonal").is_not_nan()
+                & pl.col("error_covariance_diagonal").is_not_null()
+            )
             print(mon_df)
 
             # count obs per grid for output
-            gridbox_counts = mon_df["gridbox"].value_counts()
-            gridbox_count_np = gridbox_counts.to_numpy()
-            gridbox_id_np = gridbox_counts.index.to_numpy()
-            del gridbox_counts
-            water_mask = np.copy(mesh_lat)
-            grid_obs_2d = krig.result_reshape_2d(
-                gridbox_count_np, gridbox_id_np, water_mask
+            gridbox_counts = mon_df["grid_idx"].value_counts()
+            grid_obs_2d = assign_to_grid(
+                gridbox_counts["count"].to_numpy(),
+                gridbox_counts["grid_idx"].to_numpy(),
+                output_grid,
             )
-
-            # need to either add weights (which will be just 1 everywhere as obs are gridded)
+            # need to either add weights (which will be just 1 everywhere as
+            # obs are gridded)
             # krige obs onto gridded field
-            W = obs_module.get_weights(mon_df)
+            W = get_weights(mon_df)
             # print(W)
 
-            grid_idx = np.array(sorted(mon_df["gridbox"]))  # sorted?
+            grid_idx = mon_df.get_column("grid_idx").to_numpy()
             # print(error_covariance, error_covariance.shape)
             error_covariance = error_covariance[
                 grid_idx[:, None], grid_idx[None, :]
@@ -420,14 +483,14 @@ def main():  # noqa: D103
 
             print(mon_df)
 
-            anom, uncert = krig.kriging(
+            anom, uncert = kriging(
                 grid_idx,
                 W,
-                mon_df[hadcrut_var].values,
+                mon_df.get_column(hadcrut_var).to_numpy(),
                 interp_covariance,
                 error_covariance,
-                method=args.method,
-                remove_obs_mean=args.remove_obs_mean,
+                method=method,
+                remove_obs_mean=remove_obs_mean,
             )
             print("Kriging done, saving output")
             print(anom)
