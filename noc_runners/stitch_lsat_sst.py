a81efaf139cd4413754463cbd36a83268db50777
josidd <joseph.siddons@noc.ac.uk>
2025-02-20 12:01:49 +0000

    fix: use polars to ensure coordinates align

--- noc_runners/stitch_lsat_sst.py
+++ noc_runners/stitch_lsat_sst.py
@@ -10,9 +10,10 @@ For Python >= 3.11
 import os
 
 import yaml
+import polars as pl
+import xarray as xr
 
-from glomar_gridding.array import merge_by_weights
-from glomar_gridding.io import load_array
+from glomar_gridding.io import load_array, load_dataset
 
 CONFIG_PATH: str = os.path.join(os.path.dirname(__file__), "config_stitch.yaml")
 ENSEMBLES: list[int] = list(range(1, 201))
@@ -37,33 +38,70 @@ def main() -> None:  # noqa: D103
     weights_path: str = config.get("weights_path", "")
     weights_var_name: str = config.get("weights_var_name", "weights")
 
-    weights = load_array(weights_path, weights_var_name)
-    weights = weights.sel(
-        YEAR_RANGE[0] <= weights.time.dt.year <= YEAR_RANGE[1]
+    weights = (
+        pl.from_pandas(
+            load_array(weights_path, weights_var_name)
+            .to_dataframe()
+            .reset_index()
+        )
+        .rename({"latitude": "lat", "longitude": "lon"})
+        .filter(pl.col("time").dt.year().is_between(*YEAR_RANGE, closed="both"))
+        .with_columns(pl.col("time").dt.replace(day=15, hour=12).name.keep())
     )
 
     weight_indicator: str = config.get("weight_indicator", "sst")
 
     for member in ENSEMBLES:
-        sst_array = load_array(sst_path, var=sst_var_name, member=member)
-        lsat_array = load_array(lsat_path, var=lsat_var_name, member=member)
+        print(f"Doing ensemble member: {member}")
+        sst_array = (
+            pl.from_pandas(
+                load_dataset(sst_path, member=member)
+                .to_dataframe()
+                .reset_index()
+            )
+            .filter(
+                pl.col("time").dt.year().is_between(*YEAR_RANGE, closed="both")
+            )
+            .with_columns(
+                pl.col("time").dt.replace(day=15, hour=12).name.keep()
+            )
+            .rename({"epsilon": "sst_epsilon", "n_obs": "sst_n_obs"})
+        )
+        lsat_array = (
+            pl.from_pandas(
+                load_dataset(lsat_path, member=member)
+                .to_dataframe()
+                .reset_index()
+            )
+            .filter(
+                pl.col("time").dt.year().is_between(*YEAR_RANGE, closed="both")
+            )
+            .with_columns(
+                pl.col("time").dt.replace(day=15, hour=12).name.keep()
+            )
+            .rename({"epsilon": "lsat_epsilon", "n_obs": "lsat_n_obs"})
+        )
+        coords = ["time", "lat", "lon"]
+
+        combined_df = sst_array.join(lsat_array, on=coords, how="left")
+        combined_df = combined_df.join(weights, on=coords, how="left")
 
         out_array_member_path = out_path.format(member=member)
 
         match weight_indicator:
             case "sst":
-                combined = merge_by_weights(
-                    left=sst_array,
-                    right=lsat_array,
-                    weights=weights,
-                    output_name=out_var_name,
+                combined_df = combined_df.with_columns(
+                    (
+                        pl.col(sst_var_name) * pl.col(weights_var_name)
+                        + (1 - pl.col(weights_var_name)) * pl.col(lsat_var_name)
+                    ).alias(out_var_name)
                 )
             case "lsat":
-                combined = merge_by_weights(
-                    left=lsat_array,
-                    right=sst_array,
-                    weights=weights,
-                    output_name=out_var_name,
+                combined_df = combined_df.with_columns(
+                    (
+                        pl.col(lsat_var_name) * pl.col(weights_var_name)
+                        + (1 - pl.col(weights_var_name)) * pl.col(sst_var_name)
+                    ).alias(out_var_name)
                 )
             case _:
                 raise ValueError(
@@ -71,7 +109,11 @@ def main() -> None:  # noqa: D103
                     + "'sst' or 'lsat'"
                 )
 
-        combined.to_netcdf(out_array_member_path)
+        combined_df = combined_df.sort(coords)
+
+        xr.Dataset.from_dataframe(
+            combined_df.to_pandas().set_index(coords)
+        ).to_netcdf(out_array_member_path)
 
     return None
 
