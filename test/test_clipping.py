8b622b6cb17df8c62260aba09357f1815fb00292
Steven Chan <steven.chan@noc.ac.uk>
2025-03-05 17:17:32 +0000

    Updated test_clipping tests

--- test/test_clipping.py
+++ test/test_clipping.py
@@ -1,7 +1,9 @@
 import pytest
 import iris
 import numpy as np
+from scipy import linalg
 from ellipse_estimation import repair_damaged_covariance as rdc
+from ellipse_estimation import simulate_ellipse as s_e
 
 sst_cov_path = '/noc/mpoc/surface_data/ESA_CCI5deg_month_extra/ANOMALY/SpatialScales/locally_build_covariances/'
 
@@ -45,15 +47,26 @@ def test_clipping(unclipped, clipped):
     print('Test 3')
     cleaned_cov_via_cov_ev95 = cleaner.eig_clip_via_cov()
     print(cleaned_cov_via_cov_ev95)
+    # Check explained variance to be the same
+    print('Check for nearly unchanged explained variance')
     print(np.trace(cleaned_cov_via_cov_ev95))
     print(np.abs(np.trace(cleaned_cov_via_cov_ev95)/np.trace(unclipped_cov_cube.data)-1.0),
           np.abs(np.trace(cleaned_cov_via_cov_ev95)/np.trace(unclipped_cov_cube.data)-1.0) < 1E-5)
     assert np.abs(np.trace(cleaned_cov_via_cov_ev95)/np.trace(unclipped_cov_cube.data)-1.0) < 1E-5
-    # #
-    # # This is just checking, we know they should be the same!
-    # print('Final Check')
-    # print(cleaned_cov_via_cov_ev95)
-    # print(clipped_cov_cube.data)
-    # max_diff = np.max(np.abs(cleaned_cov_via_cov_ev95 - clipped_cov_cube.data))
-    # print(max_diff)
-    # assert np.max(np.abs(cleaned_cov_via_cov_ev95 - clipped_cov_cube.data)) < 1E-5
+    # Check no negative eigenvalues remain
+    print('Check all eigenvalues are positive')
+    eigv5 = linalg.eigh(cleaned_cov_via_cov_ev95, eigvals_only=True, subset_by_index=[0,4])
+    print(f'5 smallest eigenvalues = {eigv5}')
+    assert np.all(eigv5 > 0)
+    # This is just checking, we know they should be the same!
+    print('Final Check: against a known answer')
+    print(cleaned_cov_via_cov_ev95)
+    print(clipped_cov_cube.data)
+    max_diff = np.max(np.abs(cleaned_cov_via_cov_ev95 - clipped_cov_cube.data))
+    print(max_diff)
+    assert np.max(np.abs(cleaned_cov_via_cov_ev95 - clipped_cov_cube.data)) < 1E-5
+    W, p_val = s_e.chisq_test_using_likelihood_ratios_4_covariance(cleaned_cov_via_cov_ev95,
+                                                                   clipped_cov_cube.data,
+                                                                   40)
+    print(W, p_val)
+    assert p_val > 0.99
