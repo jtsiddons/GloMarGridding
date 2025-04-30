9657fc6d91530c1b608af6f1e0bd5edf48a0f7de
josidd <joseph.siddons@noc.ac.uk>
2025-04-30 10:45:17 +0100

    rm: remove non-library files

--- ellipse_estimation/run_scripts/ellipse_mle_LSAT/clipping/clip_lsat_cov.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-setenv OMP_NUM_THREADS 8
-setenv OPENBLAS_NUM_THREADS 8
-setenv MKL_NUM_THREADS 8
-setenv VECLIB_MAXIMUM_THREADS 8
-setenv NUMEXPR_NUM_THREADS 8
-python -u clip_lsat_cov.py
-echo 'Complete'
