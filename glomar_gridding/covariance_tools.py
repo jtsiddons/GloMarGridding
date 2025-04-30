2f6cf4f24ad2257228f2f98e6b1e7dfcc73d4321
josidd <joseph.siddons@noc.ac.uk>
2025-04-30 13:41:57 +0100

    feat: add simple_clipping function

--- glomar_gridding/covariance_tools.py
+++ glomar_gridding/covariance_tools.py
@@ -14,7 +14,7 @@ rounding errors to have this occur
 In our cases -- 2 and 3...?
 
 Fixes:
-1) Simple clipping:
+1) Simple clipping - `glomar_gridding.covariance_tools.simple_clipping`:
 Cut off the negative, zero, and small positive eigenvalues;
 this is method used in statsmodels.stats.correlation_tools
 but the version here has better thresholds based on
@@ -32,7 +32,7 @@ It is more difficult to use for covariance matrices with one large dominant mode
 because that raises the bar of accuracy of the eigenvalues, so you have to clip
 off a lot more eigenvectors.
 
-2) Better (original) clipping:
+2) Original clipping - `glomar_gridding.covariance_tools.eigenvalue_clip`:
 Determine a noise eigenvalue threshold and
 replace all eigenvalues below using the average of them,
 preserving the original trace (aka total variance) of the
@@ -40,7 +40,7 @@ covariance matrix, but this will require a full computation
 of all eigenvectors, which may be slow and cause
 memory problems
 
-3) EOF chop-off (fastest, trace/variance non-conserving):
+3) EOF chop-off - `glomar_gridding.covariance_tools.eof_chop`:
 Set a target explained variance (say 95%) for the empirical
 orthogonal functions, compute the eigenvalues and
 eigenvectors up to that explained variance. Reconstruct
@@ -226,30 +226,62 @@ def check_symmetric(
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
 
-def perturb_sym_matrix_2_positive_definite(
-    square_sym_matrix: np.ndarray,
+def perturb_cov_to_positive_definite(
+    cov: np.ndarray,
+    threshold: float | Literal["auto"] = 1e-15,
 ) -> np.ndarray:
     """
-    On the fly eigenvalue clipping, this is based statsmodels code
-    statsmodels.stats.correlation_tools.cov_nearest
-    statsmodels.stats.correlation_tools.corr_nearest
+    Force an estimated covariance matrix to be positive definite using the
+    eigenvalue clipping with statsmodels.stats.correlation_tools.cov_nearest
+    function.
 
-    Use repair_damaged_covariance instead, it is more complete
+    Deprecated in favour of glomar_gridding.covariance_tools.simple_clipping.
 
-    Other methods exist:
-    https://nhigham.com/2021/02/16/diagonally-perturbing-a-symmetric-matrix-to-make-it-positive-definite/
-    https://nhigham.com/2013/02/13/the-nearest-correlation-matrix/
-    https://academic.oup.com/imajna/article/22/3/329/708688
+    Parameters
+    ----------
+    cov : numpy.ndarray
+        The estimated covariance matrix that is not positive definite.
+    threshold : float | 'auto'
+        Eigenvalues below this value are set to 0. If the input is 'auto' then
+        the value is determined using the floating-point precision and magnitude
+        of the largest eigenvalues.
+
+    Returns
+    -------
+    cov_adj : numpy.ndarray
+        Adjusted covariance matrix
+
+    See Also
+    --------
+    Use simple_clipping instead.
+
+    Notes
+    -----
+    Other methods:
+        https://nhigham.com/2021/02/16/diagonally-perturbing-a-symmetric-matrix-to-make-it-positive-definite/
+        https://nhigham.com/2013/02/13/the-nearest-correlation-matrix/
+        https://academic.oup.com/imajna/article/22/3/329/708688
     """
-    matrix_dim = square_sym_matrix.shape
+    warn(
+        "This function is deprecated in favour of "
+        + "'glomar_gridding.covariance_tool.simple_clipping'",
+        DeprecationWarning,
+    )
+    matrix_dim = cov.shape
     if (
         (len(matrix_dim) != 2)
         or (matrix_dim[0] != matrix_dim[1])
-        or not check_symmetric(square_sym_matrix)
+        or not check_symmetric(cov)
     ):
         raise ValueError("Matrix is not square and/or symmetric.")
 
-    eigenvalues = np.linalg.eigvalsh(square_sym_matrix)
+    eigenvalues = np.linalg.eigvalsh(cov)
+    if threshold == "auto":
+        finfo = np.finfo(eigenvalues.dtype)
+        threshold = 5.0 * finfo.resolution * np.max(np.abs(eigenvalues))
+    if not isinstance(threshold, (float, int)):
+        raise TypeError("`threshold` must be numeric, or 'auto'.")
+
     min_eigen = np.min(eigenvalues)
     max_eigen = np.max(eigenvalues)
     n_negatives = np.sum(eigenvalues < 0.0)
@@ -259,16 +291,18 @@ def perturb_sym_matrix_2_positive_definite(
     print("Smallest eigenvalue = ", min_eigen)
     if min_eigen >= 0.0:
         print("Matrix is already positive (semi-)definite.")
-        return square_sym_matrix
-    perturbed = correlation_tools.cov_nearest(
-        square_sym_matrix, return_all=False
+        return cov
+    cov_adj = correlation_tools.cov_nearest(
+        cov,
+        return_all=False,
+        threshold=threshold,
     )
-    if not isinstance(perturbed, np.ndarray):
+    if not isinstance(cov_adj, np.ndarray):
         raise TypeError(
             "Output of correlation_tools.cov_nearest is not a numpy array"
         )
 
-    eigenvalues_adj = np.linalg.eigvalsh(perturbed)
+    eigenvalues_adj = np.linalg.eigvalsh(cov_adj)
     min_eigen_adj = np.min(eigenvalues_adj)
     max_eigen_adj = np.max(eigenvalues_adj)
     n_negatives_adj = np.sum(eigenvalues_adj < 0.0)
@@ -276,7 +310,188 @@ def perturb_sym_matrix_2_positive_definite(
     print("Number of negative eigenvalues (post_adj) = ", n_negatives_adj)
     print("Largest eigenvalue (post_adj)  = ", max_eigen_adj)
     print("Smallest eigenvalue (post_adj) = ", min_eigen_adj)
-    return perturbed
+    return cov_adj
+
+
+def simple_clipping(
+    cov,
+    threshold: float | Literal["auto", "statsmodels_default"] = "auto",
+    method: Literal["iterative", "direct"] = "iterative",
+) -> tuple[np.ndarray, dict[str, Any]]:
+    """
+    A modified version of:
+    https://www.statsmodels.org/dev/generated/statsmodels.stats.correlation_tools.corr_nearest.html
+
+    Force an estimated covariance matrix to be positive definite using the
+    eigenvalue clipping with statsmodels.stats.correlation_tools.cov_nearest
+    function.
+
+    This is appropriate for covariance matrices which have less dominant modes;
+    it also preserves more noise.
+
+    Trace (aka total variance) of the covariance matrix is not conserved,
+    but it is less disruptive than EOF chop off.
+
+    Parameters
+    ----------
+    cov : numpy.ndarray
+        The estimated covariance matrix that is not positive definite.
+    threshold : float | 'auto'
+        Eigenvalues below this value are set to 0. If the input is 'auto' then
+        the value is determined using the floating-point precision and magnitude
+        of the largest eigenvalues.
+
+    Returns
+    -------
+    cov_adj : numpy.ndarray
+        Adjusted covariance matrix
+    summary_dict : dict[str, Any]
+        A dictionary containing a summary of the input and results with the
+        following keys:
+            threshold
+            smallest_eigv
+            determinant
+            total_variance
+
+    See Also
+    --------
+    statsmodels.stats.correlation_tools.cov_nearest
+
+    Notes
+    -----
+    Other methods:
+        https://nhigham.com/2021/02/16/diagonally-perturbing-a-symmetric-matrix-to-make-it-positive-definite/
+        https://nhigham.com/2013/02/13/the-nearest-correlation-matrix/
+        https://academic.oup.com/imajna/article/22/3/329/708688
+    """
+    all_eigval = np.linalg.eigvals(cov)
+    all_eigval = np.sort(all_eigval)
+    max_eigval = np.max(all_eigval)
+    min_eigval = np.min(all_eigval)
+    sum_eigval = np.sum(all_eigval)
+    p90_index = int(0.1 * len(all_eigval))
+    sumtop10_eigval = np.sum(all_eigval[-p90_index:])
+    top10_explained_var = 100.0 * (sumtop10_eigval / sum_eigval)
+    print("Pre-adjusted eigenvalue summary")
+    print("Largest=", max_eigval)
+    print("Smallest/most negative=", min_eigval)
+    print("Sum=", sum_eigval)
+    print("Explained variance from top 10%=", top10_explained_var, "%")
+
+    if threshold == "auto":
+        # According to
+        # https://stackoverflow.com/questions/13891225/precision-of-numpys-eigenvaluesh
+        # https://www.netlib.org/lapack/lug/node89.html
+        #
+        # Accuracy of eigenvalue of lapack is greater or equal to
+        # MAX(ABS(eigenvalues)) x floating_pt_accuracy of the float type
+        #
+        # e.g. for float32 np.array:
+        # np.finfo(np.float32) -->
+        # finfo(
+        #     resolution=1e-06,
+        #     min=-3.4028235e38,
+        #     max=3.4028235e38,
+        #     dtype=float32,
+        # )
+        # Typical SSTA covariance matricies has max eigv ~ 1000 degC**2
+        # This gives a lower bound threshold on the order of 1E-6 x 1E3 ~ 1E-3
+        #
+        # Give some margin of safety to above approximation: we will do 5x of
+        # the above
+        # For most climate science applications,
+        # these eigenvalues are essentially noise to the data
+        finfo = np.finfo(all_eigval.dtype)
+        threshold = 5.0 * finfo.resolution * np.max(np.abs(all_eigval))
+    elif threshold == "statsmodels_default":
+        # 1e-15 is the precision for np.float64
+        # This is the default used in
+        # https://www.statsmodels.org/dev/generated/statsmodels.stats.correlation_tools.corr_clipped.html
+        #
+        # It is NOT SUITABLE based on guidelines to the precision of
+        # lapack eigenvalue decomposition, nor the input data can be assumed
+        # to be float64!
+        threshold = 1e-15
+    if not isinstance(threshold, (float, int)):
+        raise TypeError(
+            "threshold must either be number, auto or statsmodels_default"
+        )
+
+    n_negative = int(np.sum(all_eigval < threshold))
+    print("Minimum eigenvalue threshold = ", threshold)
+    print("Estimated number of eigenvalues below threshold = ", n_negative)
+    n_vec = n_negative
+
+    print(
+        "Computing eigenvalues and eigenvector up to the estimated number: "
+        + str(n_vec)
+    )
+    current_eigv, current_eigV = sc.linalg.eigh(
+        cov, eigvals_only=False, subset_by_index=[0, n_vec - 1]
+    )
+    print(current_eigv)
+    print(current_eigV)
+    print(current_eigV.shape)
+
+    # Make a copy
+    cov_adj = cov.copy()
+
+    # Rank-n_vec update
+    print("Fixing matrix by reverse clipping")
+    if method == "iterative":
+        for iii in range(n_vec):
+            if current_eigv[iii] > threshold:
+                warning_msg = "New estimate of eigenvalue is below threshold,"
+                warning_msg += "possibly due to precision; bypassing."
+                print((iii, n_vec - 1, current_eigv[iii], warning_msg))
+                continue
+            worst_eigV = current_eigV[:, iii][np.newaxis]
+            VbadxVbadT = worst_eigV * worst_eigV.T
+            # This is only if threshold == 0
+            # r_peturb = VbadxVbadT * current_eigv[iii]
+            # cov_arr_adj = cov_arr_adj - r_peturb
+            r_peturb = VbadxVbadT * (threshold - current_eigv[iii])
+            cov_adj = cov_adj + r_peturb
+            print(
+                (
+                    iii,
+                    n_vec - 1,
+                    current_eigv[iii],
+                    threshold - current_eigv[iii],
+                    np.max(np.diag(r_peturb)),
+                )
+            )
+    elif method == "direct":
+        dL = threshold - all_eigval[:n_vec]
+        if np.any(dL < 0.0):
+            # This might be a problem when eigenvalues are re-estimated
+            print("Some new estimates to eigenvalue are above threshold.")
+            print("No adjustments will be made for those eigenvalues")
+            dL = np.diag(np.max([np.zeros_like(dL), dL], axis=0))
+        else:
+            dL = np.diag(dL)
+        dC = np.matmul(np.matmul(current_eigV, dL), current_eigV.T)
+        cov_adj = cov + dC
+
+    print("Computing adjusted eigenvalues, smallest " + str(n_vec))
+    new_eigv = sc.linalg.eigh(
+        cov_adj, eigvals_only=True, subset_by_index=[0, n_vec - 1]
+    )
+    new_min_eigv = np.min(new_eigv)
+    print("Eigenvalues that were smaller than threshold:")
+    print("[:5] : ", new_eigv[:5])
+    print("[-5:]: ", new_eigv[-5:])
+    print("Smallest eigenvalue=", new_min_eigv)
+    new_det = np.linalg.det(cov_adj)
+    print("Determinant=", new_det)
+    total_var = np.sum(np.diag(cov_adj))
+    meta_dict = {
+        "threshold": threshold,
+        "smallest_eigv": new_min_eigv,
+        "determinant": new_det,
+        "total_variance": total_var,
+    }
+    return (cov_adj, meta_dict)
 
 
 def csum_up_to_val(
@@ -380,17 +595,16 @@ def _find_index_aspect_ratio(
 ) -> int:
     """
     Defaults are based on:
-    41 years ESA data
-    6 pentads per month
-    and 37000ish 1x1 deg grid points
-    q ~ 150, threshold ~ 175
+        41 years ESA data
+        6 pentads per month
+        and 37000ish 1x1 deg grid points
+    Resulting in q ~ 150, threshold ~ 175
 
     For 5x5 data and 40-ish year of observations
-    Observations are monthly
-    q ~ 65, threshold ~ 82
+    Observations are monthly:
+        q ~ 65, threshold ~ 82
 
-    These parameters do not work in general
-    must be determined from input data
+    These parameters do not work in general must be determined from input data.
     """
     _, threshold = _estimate_threshold(num_grid_pts, num_times)
     return -int(np.sum(eigvals > threshold))
@@ -413,16 +627,16 @@ def _estimate_threshold(
 
 
 def eigenvalue_clip(
-    mat: np.ndarray,
+    cov: np.ndarray,
     method: Literal["explained_variance", "Laloux_2000"] = "explained_variance",
     method_parms={"target": 0.95},
-):
+) -> np.ndarray:
     """
-    Denoise symmetric damaged covariance/correlation matrix C
-    by clipping eigenvalues
+    Denoise symmetric damaged covariance/correlation matrix cov by clipping
+    eigenvalues
 
-    This is the original method (?)
-    https://www.worldscientific.com/doi/abs/10.1142/S0219024900000255
+    This is the original method:
+        https://www.worldscientific.com/doi/abs/10.1142/S0219024900000255
 
     Explained variance or aspect ratio based threshold
     Aspect ratios is based on dimensionless parameters
@@ -440,8 +654,20 @@ def eigenvalue_clip(
     in which (number of basis vectors to be kept) >> (number of rows)
     In ESA data, keeping 95% variance means keeping top ~15% of the
     eigenvalues
+
+    Parameters
+    ----------
+    cov : numpy.ndarray
+        Input covariance matrix to be adjusted to positive definite.
+    method : "explained_variance" | "Laloux_2000"
+        Method used to identify the index of the eigenvalues to clip.
+
+    Returns
+    -------
+    cov_adj : numpy.ndarray
+        Adjusted covariance matrix.
     """
-    eigvals, eigvecs = np.linalg.eigh(mat)
+    eigvals, eigvecs = np.linalg.eigh(cov)
     print("[:5] =", eigvals[:5])
     print("[-5:] =", eigvals[-5:])
     n_eigvals = len(eigvals)
