7bce05bde805b051eb3fd76735218a6cb8922f1b
josidd <joseph.siddons@noc.ac.uk>
2025-05-29 07:25:08 +0100

    test: variogram tests

---
+++ test/test_variogram.py
@@ -0,0 +1,116 @@
+import pytest
+import numpy as np
+
+
+from glomar_gridding.grid import grid_from_resolution, grid_to_distance_matrix
+from glomar_gridding.variogram import (
+    MaternVariogram,
+    GaussianVariogram,
+    ExponentialVariogram,
+    variogram_to_covariance,
+)
+
+
+GRID = grid_from_resolution(
+    5, [(-25, 25), (-25, 25)], ["latitude", "longitude"]
+)
+
+DIST = grid_to_distance_matrix(
+    GRID, lat_coord="latitude", lon_coord="longitude"
+).values
+
+
+@pytest.mark.parametrize(
+    "variogram_model, parameters, variance",
+    [
+        (
+            GaussianVariogram,
+            {"psill": 1.2, "nugget": 0, "range": 1200},
+            1.2,
+        ),
+        (
+            GaussianVariogram,
+            {"psill": 1.2, "nugget": 0.101, "range": 1200},
+            1.2,
+        ),
+        (
+            ExponentialVariogram,
+            {"psill": 1.2, "nugget": 0, "range": 1200},
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0,
+                "range": 1200,
+                "nu": 1.5,
+                "method": "sklearn",
+            },
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0,
+                "range": 1200,
+                "nu": 0.5,
+                "method": "sklearn",
+            },
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0,
+                "range": 1200,
+                "nu": 3.0,
+                "method": "sklearn",
+            },
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0,
+                "range": 1200,
+                "nu": 1.5,
+                "method": "gstat",
+            },
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0,
+                "range": 1200,
+                "nu": 1.5,
+                "method": "karspeck",
+            },
+            1.2,
+        ),
+        (
+            MaternVariogram,
+            {
+                "psill": 1.2,
+                "nugget": 0.105,
+                "range": 1200,
+                "nu": 1.5,
+                "method": "karspeck",
+            },
+            1.2,
+        ),
+    ],
+)
+def test_variogram(variogram_model, parameters, variance):
+    # TEST: Generates a valid covariance
+    variogram = variogram_model(**parameters)
+    variogram_result = variogram.fit(DIST)
+    covariance = variogram_to_covariance(variogram_result, variance)
+
+    evals = np.linalg.eigvalsh(covariance)
+    assert (evals > 0).all()
