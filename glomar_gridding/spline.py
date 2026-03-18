fix(spline): adjustments to variance fitting

glomar_gridding/spline.py
@@ -158,7 +158,7 @@ class Spline(ABC):
         c: np.ndarray | NoneType = None,
     ) -> np.ndarray:
         """Solve"""
-        # "^" is XOR (Exclusive OR)
+        # "^" is XOR (Exclusive OR - True if EXACTLY one condition is True)
         if (w is None) ^ (c is None):
             raise ValueError()
 
@@ -167,7 +167,7 @@ class Spline(ABC):
 
         if w is None:
             if not hasattr(self, "w") or lam != self.lam:
-                w, c = self.fit(lam=lam, set_results=True)  # type: ignore
+                w, c = self.fit(lam=lam, set_results=True)
             else:
                 w = self.w
                 c = self.c
@@ -182,11 +182,13 @@ class Spline(ABC):
         """Get variance"""
         residual = self.y - y_fit
 
-        tmp = np.linalg.inv(self.K_reg)
+        inv_K_reg = np.linalg.inv(self.K_reg)
+        K_inv_K_reg = self.K @ inv_K_reg
+        del inv_K_reg
 
-        df: float = self.n_pts - np.trace(self.K_reg @ tmp)
+        df: float = self.n_pts - np.trace(K_inv_K_reg)
         sigma2: float = np.sum(residual**2) / df
-        H_diag: np.ndarray = np.sum(self.K_reg * tmp, axis=1)
+        H_diag: np.ndarray = np.sum(K_inv_K_reg @ self.K.T, axis=1)
 
         var_y_fit: np.ndarray = sigma2 * H_diag
 
@@ -234,10 +236,10 @@ class Spline(ABC):
 
         Parameters
         ----------
-        lambda_bounds : tuple[float, float]
-            Bounds on the optimisation for lambda.
         set_result : bool
             Store the result as the 'lam' attribute.
+        **kwargs
+            Keyword arguments to pass to minimize_scalar.
 
         Returns
         -------

test/test_spline.py
@@ -91,7 +91,10 @@ def test_var() -> None:
     )
 
     tps = ThinPlateSpline(X=X, y=y)
-    _ = tps.estimate_lambda_gcv()
+    lam = 0.025
+    # tps.lam = lam
+    lam = tps.estimate_lambda_gcv()
+    print(f"{lam = }")
     tps.fit()
     y_pred = tps.solve()
     sigma, var = tps.fit_variance(y_pred)
@@ -101,3 +104,21 @@ def test_var() -> None:
 
     assert isinstance(var, np.ndarray)
     assert np.all(var > 0)
+
+
+# def test_gcv_loss() -> None:
+#     np.random.seed(314159)
+#     n_samps = 50
+#
+#     X = 1 - 2 * np.random.rand(n_samps, 2)
+#     X[:, 0] *= np.pi / 2
+#     X[:, 1] *= np.pi
+#     y = 0.5 - (
+#         np.sin(2 * X[:, 0])
+#         + np.cos(2 * X[:, 1])
+#         + 0.1 * np.random.randn(n_samps)
+#     )
+#
+#     tps = ThinPlateSpline(X=X, y=y)
+#     lam = tps.estimate_lambda_gcv(method="bounded", bounds=(1e-6, 1e2))
+#     print(f"{lam = }")