39816e9d5904319ccf356acb83daa54175867b1d
Steven Chan <steven.chan@noc.ac.uk>
2026-06-12 16:02:03 +0100

    Moved weight/autocorrelation estimation to new dir - __init__.py created for that directory

---
+++ glomar_gridding/autocorrelation/__init__.py
@@ -0,0 +1,23 @@
+"""Functions and methods to estimate autoregression weights"""
+
+from glomar_gridding.autocorrelation.compute_autocorrelation import (
+    get_auto_corr,
+    get_auto_corr_1_vectorize,
+)
+
+from glomar_gridding.autocorrelation.lasso_estimate import (
+    LassoEstimate_AR1,
+)
+
+from glomar_gridding.autocorrelation.lasso_postprocessing import (
+    LassoWeights,
+    LassoError,
+)
+
+__all__ = [
+    "LassoError",
+    "LassoEstimate_AR1",
+    "LassoWeights",
+    "get_auto_corr",
+    "get_auto_corr_1_vectorize",
+]
