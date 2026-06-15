230ed0347597686f51b1bd686ed9a4420d5c2f17
Steven Chan <steven.chan@noc.ac.uk>
2026-06-15 13:18:47 +0100

    Modified to asserts somewhat to be more precise - removed the correlation between two columns for 2nd test

--- test/test_ar_weights.py
+++ test/test_ar_weights.py
@@ -8,21 +8,33 @@ from glomar_gridding.autocorrelation import (
 
 
 def test_time_series():
+    # It has been Monte Carlo/stochastic simulation
+    # can be used to test anything!
     n = 99999
     phi0 = 0.6
     phi1 = 0.4
     innovation0 = 20.0
     innovation1 = 30.0
-    # (Opening day of NOCS and UKMO Exeter...)
+    # Opening day of NOCS and UKMO Exeter...
     seed = 19942003
+    #
+    # Simulate 4 temporally correlated time series
+    # x0 and x1 used for all unit tests
+    # x2 and x3 used for the second unit test
     rng = np.random.default_rng(seed)
     epsilon0 = rng.normal(loc=0, scale=innovation0, size=n)
     epsilon1 = rng.normal(loc=0, scale=innovation1, size=n)
+    epsilon2 = rng.normal(loc=0, scale=innovation0, size=n)
+    epsilon3 = rng.normal(loc=0, scale=innovation1, size=n)
     x0 = np.zeros(n + 1)
     x1 = np.zeros(n + 1)
+    x2 = np.zeros(n + 1)
+    x3 = np.zeros(n + 1)
     for t in range(n):
         x0[t + 1] = phi0 * x0[t] + epsilon0[t]
         x1[t + 1] = phi1 * x1[t] + epsilon1[t]
+        x2[t + 1] = phi0 * x2[t] + epsilon2[t]
+        x3[t + 1] = phi1 * x3[t] + epsilon3[t]
     #
     # Other info:
     # sigma(x0) = SQRT( (20 x 20) / (1 - 0.6 x 0.6) ) ~ 25.0
@@ -32,12 +44,10 @@ def test_time_series():
     x0_variance_theory = (innovation0**2) / (1 - phi0**2)
     x1_variance_theory = (innovation1**2) / (1 - phi1**2)
     #
-    # Shape should (100000, 2)
-    x_matrix = np.column_stack([x0, x1])
-    # print(x_matrix.shape)
-    ans = get_auto_corr_1_vectorize(x_matrix)
+    x_matrix1 = np.column_stack([x0, x1])
+    x_matrix2 = np.column_stack([x2, x3])
+    ans = get_auto_corr_1_vectorize(x_matrix1)
     simulated_ar_coefficients = ans[0]
-    # simulated_autocovariance = ans[1]
     simulated_variance = ans[2]
     #
     # Standard error of correlation coefficient is SQRT( (1 - r x r) / (n - 2) )
@@ -60,38 +70,40 @@ def test_time_series():
         chi2_test_stat_upper_bound > chi2_test_stat,
         chi2_test_stat > chi2_test_stat_lower_bound,
     )
-    # print(chi2_test)
     #
-    # For the above seed: it is known to pass the below checks
+    # For the above seed, it is known to pass the below two unit tests
     #
     # Simulated AR should be within 1 standard error
     assert np.all(delta_ar < (1.0 * se_ar))
     # You have to be quite unlucky for the simulated variance to
-    # sit outside the 99% two tail test...
+    # sit outside the 99% two-tail test... and it doesn't for this
+    # particular seed
     assert np.all(chi2_test)
     #
-    # Let make 2 time series correlated...
-    cov_x0_x1 = x_matrix.T @ x_matrix / (n - 1)
-    sigma_x0_x1 = np.sqrt(np.diag(cov_x0_x1))
-    mean_x0_x1 = np.mean(x_matrix, axis=0)
-    x_matrix_standardised = x_matrix.copy()
+    # Make x2 and x3 correlated with each other
+    cov_x2_x3 = x_matrix2.T @ x_matrix2 / (n - 1)
+    sigma_x2_x3 = np.sqrt(np.diag(cov_x2_x3))
+    mean_x0_x1 = np.mean(x_matrix2, axis=0)
+    x_matrix_standardised = x_matrix2.copy()
     x_matrix_standardised[:, 0] = (
         x_matrix_standardised[:, 0] - mean_x0_x1[0]
-    ) / sigma_x0_x1[0]
+    ) / sigma_x2_x3[0]
     x_matrix_standardised[:, 1] = (
         x_matrix_standardised[:, 1] - mean_x0_x1[1]
-    ) / sigma_x0_x1[1]
-    adj_cov_x0_x1 = (
-        np.diag(sigma_x0_x1)
+    ) / sigma_x2_x3[1]
+    #
+    # Use Cholesky to make them correlated
+    adj_cov_x2_x3 = (
+        np.diag(sigma_x2_x3)
         @ np.array(
             [
                 [1.0, 0.7],
                 [0.7, 1.0],
             ]
         )
-        @ np.diag(sigma_x0_x1)
+        @ np.diag(sigma_x2_x3)
     )
-    l_cov = np.linalg.cholesky(adj_cov_x0_x1)
+    l_cov = np.linalg.cholesky(adj_cov_x2_x3)
     x_matrix_correlated = l_cov.dot(x_matrix_standardised.T)
     x_matrix_correlated = x_matrix_correlated.T
     #
@@ -99,8 +111,8 @@ def test_time_series():
         [
             x_matrix_correlated[:, 0],
             x_matrix_correlated[:, 1],
-            x_matrix[:, 0],
-            x_matrix[:, 1],
+            x_matrix1[:, 0],
+            x_matrix1[:, 1],
         ]
     )
     #
@@ -113,8 +125,27 @@ def test_time_series():
     )
     lasso.fit()
     #
-    # Note: W = cov(x(t+1), x(t)) @ inv(cov(x, x))
-    # observed_cov_x_x = big_x.T @ big_x / (n - 1)
+    # Note:
+    # W = cov(x(t+1), x(t)) @ inv(cov(x, x)) for OLS
+    # This is not true for Lasso in general, but should
+    # nearly be the case for this particular example
+    #
+    # For the 4 diagonal values of lasso.coefficients:
+    # 2 values should be closed to phi0 (0,0 and 2,2)
+    # 2 values should be closed to phi1 (1,1 and 3,3)
+    # lasso.coefficients[3, 2] and lasso.coefficients[2, 3] should be 
+    # close to 0 because x0 and x1 are not correlated
+    # However, either lasso.coefficients[0, 1] or lasso.coefficients[1, 0] are
+    # not close to 0 because adjusted x2 and x3 are correlated
     #
     assert np.all(np.abs(lasso.coefficients < 1.0))
-    assert np.any(np.isclose(lasso.coefficients, phi0, atol=0.05))
+    assert np.isclose(lasso.coefficients[0, 0], phi0, atol=0.01)
+    assert np.isclose(lasso.coefficients[2, 2], phi0, atol=0.01)
+    assert np.isclose(lasso.coefficients[1, 1], phi1, atol=0.01)
+    assert np.isclose(lasso.coefficients[3, 3], phi1, atol=0.01)
+    assert np.isclose(lasso.coefficients[2, 3], 0, atol=0.01)
+    assert np.isclose(lasso.coefficients[3, 2], 0, atol=0.01)
+    assert np.logical_or(
+        ~np.isclose(lasso.coefficients[0, 1], 0, atol=0.01),
+        ~np.isclose(lasso.coefficients[1, 0], 0, atol=0.01),
+    )
