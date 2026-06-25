0e4ee8aec32a4e7f098d3c6c66ef7286629a0c1a
Steven Chan <steven.chan@noc.ac.uk>
2026-06-25 15:02:29 +0100

    Remove refs to lasso_postprocessing from __init__

--- glomar_gridding/autocorrelation/__init__.py
+++ glomar_gridding/autocorrelation/__init__.py
@@ -9,15 +9,8 @@ from glomar_gridding.autocorrelation.lasso_estimate import (
     LassoEstimate_AR1,
 )
 
-from glomar_gridding.autocorrelation.lasso_postprocessing import (
-    LassoWeights,
-    LassoError,
-)
-
 __all__ = [
-    "LassoError",
     "LassoEstimate_AR1",
-    "LassoWeights",
     "get_auto_corr",
     "get_auto_corr_vectorize",
 ]
